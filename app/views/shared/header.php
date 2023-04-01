6a61b98ff6536bce4edd99ee55ba34f35c0a0352
tree <62469167+zehayk@users.noreply.github.com>
2023-04-01 12:24:32 -0400

    ZEBUEB

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -4,6 +4,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../../../stylesheets/styles.css">
+	<meta http-equiv="Cache-control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
 	<title><?= $data ?></title>
 </head>
 <body onload="openTab(event, 'parks')">
