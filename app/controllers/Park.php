cd1309a315d3c35efb4a56137b89b06d1d0fe85c
Maxym Galenko <maxymgalenko@gmail.com>
2023-04-01 10:39:34 -0400

    Created park model and controller

---
+++ app/controllers/Park.php
@@ -0,0 +1,6 @@
+<?php
+namespace app\controllers;
+
+class Park extends \app\core\Controller {
+	
+}
---
+++ app/models/Park.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\models;
+
+class Park extends \app\core\Model {
+	public $park_id;
+	public $park_name;
+	public $park_address;
+}
